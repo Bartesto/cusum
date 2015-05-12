0a21f39b2d007a97bbcd3b2133dc5161381c7277
Bartesto <barthuntley@iinet.net.au>
2015-05-12 14:34:43 +0800

    removed hard coded lngth of df dynamically coded ts start and end dates based on df

--- ver1.R
+++ ver1.R
@@ -14,7 +14,7 @@ df <- df[order(df$date),]#Very Important - order it!
 
 ##Get data into regular object (mthly) with data averaged where needed to use ts
 all <- seq(ymd('1987-02-09'), ymd('2015-10-01'), by = 'days') #daily date sequence for whole period
-d <- rep('NA', 10462) #dummy column for data frame
+d <- rep('NA', length(all)) #dummy column for data frame
 alldates <- data.frame(date = all, val = d)#make df of complete date seq
 
 df2 <- left_join(alldates, df, by = "date")#join datasets for whole seq
@@ -38,7 +38,8 @@ base.df <- df3 %>%
 #per site
 df3.i <- na.approx(df3[,4])#interpolated site 1
 base.df.i <- na.approx(base.df[,4])
-ts.d1 <- ts(base.df.i, start=c(1987, 2), end=c(2015, 10), frequency=12)
+ts.d1 <- ts(base.df.i, start=c(min(df3[,1]), as.numeric(head(df3[,2], n=1))), 
+                end=c(max(df3[,1]), as.numeric(tail(df3[,2], n=1))), frequency=12)
 
 
 
