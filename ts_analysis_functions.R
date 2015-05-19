876409aa20a83b55f80b080526f5f963981141ac
Bartesto <barthuntley@iinet.net.au>
2015-05-19 14:59:13 +0800

    separate plots now working

--- ts_analysis_functions.R
+++ ts_analysis_functions.R
@@ -19,13 +19,14 @@ rm(list=ls())
 dir="Z:\\DOCUMENTATION\\BART\\R\\R_DEV\\cusum"
 csv="dhi_site_TS_data.csv"
 base_end <- "2008-01-01"
+stdev=3
 i=1
 
 
 #libraries
 library(lubridate)
 library(ggplot2)
-
+library(tidyr)
 
 
 
@@ -51,16 +52,49 @@ seasmod.b.i <- seasmod.b.i[1:12]
 #make df of site and its model
 df2 <- df[, c(1, 1+i)]
 df2$model <- rep(seasmod.b.i, length.out = length(df[,1]))
+df2$cumsum <- cumsum(df2[,2]-df2[,3])
+df2$label <- factor(rep("cumsum", by=length(df2[,1])))
+df3 <- gather(df2, "Series", "Value", 2:3)
+vertdf <-data.frame(x=as.Date(base_end), y=c(-Inf, Inf),
+                    Mngt=factor(year(as.Date(base_end))))
+hordf <- data.frame(pos=stdev*sd(cu.base), neg=-stdev*sd(cu.base),
+                    x=c(min(df2[,1]), max(df2[,1])), Limit=factor("Limit"))
+cu.base<-cu.i[1:length(b.i)]
 
-p.i <- ggplot()+
-        geom_line(data=df2, aes(x=date, y=df2[,2], colour="blue"))+
-        geom_line(data=df2, aes(x=date, y=df2[,3], colour="red"))+
-        geom_vline(xintercept=as.numeric(as.Date(base_end)))+
-        xlab("date")+
-        ylab("cover")
-p.i             
-             
-             
-             
-             
+
+ggplot()+
+        geom_point(data=df3, aes(x=date, y=Value, colour=Series))+
+        geom_line(data=df3, aes(x=date, y=Value, colour=Series))+
+        scale_colour_manual(values=c("black", "red"),
+                          name="Series",
+                          breaks=as.character(df3$Series),
+                          labels=as.character(df3$Series))+
+        coord_cartesian(ylim = c(0, 80))+
+#         geom_line(data=df3, aes(x=date, y=series, colour="red"))+
+        #geom_vline(xintercept=as.numeric(as.Date(base_end)), linetype="dashed")+
+        geom_line(aes(x,y, linetype=Mngt), colour='blue', size = 1, vertdf)+
+        #annotate("text", 2008, max(df3$value), "Mngt Action")+
+        theme_bw()+
+        xlab("")+
+        ylab("Vegetation Cover %")
+
+
+        
+        
+        
+ggplot()+
+        geom_point(data=df2, aes(x=date, y=cumsum, colour=label))+
+        geom_line(data=df2, aes(x=date, y=cumsum, colour=label))+
+        scale_colour_manual(values="black",
+                            name="Cumsum diff \n to model",
+                            labels="")+
+        coord_cartesian(ylim = c(-1200, 1200))+
+        geom_line(aes(x,y, linetype=Mngt), colour='blue', size = 1, vertdf)+
+        geom_line(aes(x, pos), linetype="dashed", colour='red', size=1, hordf)+
+        geom_line(aes(x, neg), linetype="dashed", colour='red', size=1, hordf)+
+        geom_hline(yintercept=0)+
+        theme_bw()+
+        xlab("")+
+        ylab("Cumulative Sum")
+
              
