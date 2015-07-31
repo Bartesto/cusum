c9029dd324b228850e9a2f908d05ff8c0b952807
Bartesto <barthuntley@iinet.net.au>
2015-07-31 11:08:50 +0800

    working with new df names

--- cusum from mtsd.R
+++ cusum from mtsd.R
@@ -101,34 +101,11 @@ oca <- function(dir, csv, base_end, stdev=3, out, project){
                 cusdf.i$cumsum <- cusdf.i[,2]- cusdf.i[,3]#calc new diff to model using IRREG
                 cusdf.i[,4][is.nan(cusdf.i[,4])] <- 0#replace NaN's so cumsum works
                 cusdf.i[,4] <- cumsum(cusdf.i[,4])#run cumsum
+                cusdf.i$label <- factor(rep("cumsum", by=length(cusdf.i[,1])))
                 
                 ##New data for first plot
                 tsdf.i <- gather(cusdf.i, "Series", "Value", 2:3)
                 
-                Rdf.i[,2] <- Rdf.i[,2]*100
-                Rdf.i$model <- rep(seasmod.b.i, length.out = length(Rdf[,1]))#repeats 12 mth model
-                Rdf.i$cumsum <- cumsum(Rdf.i[,2]- Rdf.i[,3])
-                #Rdf.i$label <- factor(rep("cumsum", by=length(df2[,1])))
-                
-                df2 <- df[, c(1, 1+i)]
-                df2[,2] <- df2[,2]*100
-                df2$model <- rep(seasmod.b.i, length.out = length(df[,1]))#repeats 12 mth model
-                df2$cumsum <- cumsum(df2[,2]-df2[,3])
-                df2$label <- factor(rep("cumsum", by=length(df2[,1])))
-                
-                df3 <- gather(df2, "Series", "Value", 2:3)
-                
-                
-                
-                #Create new df for first panel plot so that model and non-interpolated data can be
-                #plotted
-                df4 <- Rdf.i #make copy of original
-                df4[,2] <- Idf3[,3+i]*100 #add in IRREG series (and convert to %)
-                df4[,4] <- df4[,2]-df4[,3]#calc new diff to model using IRREG
-                df4[,4][is.nan(df4[,4])] <- 0#replace NaN's so cumsum works
-                df4[,4] <- cumsum(df4[,4])#run cumsum
-                
-                df5 <- gather(df4, "Series", "Value", 2:3)
                 
                 #make df of vertical (mngt action) line info
                 vertdf <-data.frame(x=as.Date(base_end), y=c(-Inf, Inf),
@@ -139,57 +116,14 @@ oca <- function(dir, csv, base_end, stdev=3, out, project){
                 hordf <- data.frame(pos=stdev*sd(cu.base), neg=-stdev*sd(cu.base),
                                     x=c(min(cusdf.i[,1]), max(cusdf.i[,1])), 
                                     Limit=factor("Limit"))
-                
-                
-                #Model and ts plot
-#                 p1 <- ggplot()+
-#                         geom_point(data=df3, aes(x=date, y=Value, colour=Series))+
-#                         geom_line(data=df3, aes(x=date, y=Value, colour=Series))+
-#                         scale_colour_manual(values=c("black", "red"),
-#                                             name="Series",
-#                                             breaks=as.character(df3$Series),
-#                                             labels=as.character(df3$Series))+
-#                         coord_cartesian(ylim = c(0, 100))+
-#                         geom_line(aes(x,y, linetype=Mngt), colour='blue', size = 0.5, vertdf)+
-#                         theme_bw()+
-#                         theme(axis.text.y = element_text(angle=90))+
-#                         xlab("")+
-#                         ylab("Vegetation Cover %")
-#                 
-#                 #cusum plot
-#                 p2<- ggplot()+
-#                         geom_point(data=df2, aes(x=date, y=cumsum, colour=label))+
-#                         geom_line(data=df2, aes(x=date, y=cumsum, colour=label), size=0.5)+
-#                         scale_colour_manual(values="black",
-#                                             name="Cumsum \n diff \n to model",
-#                                             labels="")+
-#                         coord_cartesian(ylim = c(-1200, 1200))+       
-#                         geom_line(aes(x,y, linetype=Mngt), colour='blue', size = 0.5, vertdf)+
-#                         geom_line(aes(x, pos), linetype="dashed", colour='red', size=0.5, hordf)+
-#                         geom_line(aes(x, neg), linetype="dashed", colour='red', size=0.5, hordf)+
-#                         geom_hline(yintercept=0)+
-#                         annotate("text", min(df2[,1])+1000, hordf[1,1]+95, label = "Control Limits",
-#                                  size=2.5)+
-#                         theme_bw()+
-#                         theme(axis.text.y = element_text(angle=90))+
-#                         xlab("")+
-#                         ylab("Cumulative Sum")
-#                 
-#                 m <- arrangeGrob(p1,p2)
-#                 sname.i<-sname[i]
-#                 filename<-paste0(sname.i, "_orig_cusum_wil_", stdev, out)
-#                 ggsave(file=filename, m, width = 24, height = 13.5, units = "cm")
-                
-                
-                
-                
-                p3<- ggplot()+
+             
+                p1<- ggplot()+
                         geom_point(data=tsdf.i, aes(x=date, y=Value, colour=Series))+
                         geom_line(data=tsdf.i, aes(x=date, y=Value, colour=Series))+
                         scale_colour_manual(values=c("black", "red"),
                                             name="Series",
-                                            breaks=as.character(df3$Series),
-                                            labels=as.character(df3$Series))+
+                                            breaks=as.character(tsdf.i$Series),
+                                            labels=as.character(tsdf.i$Series))+
                         coord_cartesian(ylim = c(0, 100))+
                         geom_line(aes(x,y, linetype=Mngt), colour='blue', size = 0.5, vertdf)+
                         theme_bw()+
@@ -197,7 +131,7 @@ oca <- function(dir, csv, base_end, stdev=3, out, project){
                         xlab("")+
                         ylab("Vegetation Cover %")
                 
-                p4<- ggplot()+
+                p2<- ggplot()+
                         geom_point(data=cusdf.i, aes(x=date, y=cumsum, colour=label))+
                         geom_line(data=cusdf.i, aes(x=date, y=cumsum, colour=label), size=0.5)+
                         scale_colour_manual(values="black",
@@ -215,47 +149,10 @@ oca <- function(dir, csv, base_end, stdev=3, out, project){
                         xlab("")+
                         ylab("Cumulative Sum")
                 
-                o <- arrangeGrob(p3,p4)
+                o <- arrangeGrob(p1,p2)
                 sname.i<-sname[i]
-                filename<-paste0(sname.i, "_cusum_uu_", stdev, out)
+                filename<-paste0(sname.i, "_cusum_", project, "_", stdev, out)
                 ggsave(file=filename, o, width = 24, height = 13.5, units = "cm")
-                
-#                 df4$s_cumsum <- df2[,4]
-#                 df4 <- df4[,-5]
-#                 df6 <- gather(df4, "Method", "Sum", 4:5)
-#                 
-#                 p5<- ggplot()+
-#                         geom_point(data=df6, aes(x=date, y=Sum, colour=Method))+
-#                         geom_line(data=df6, aes(x=date, y=Sum, colour=Method))+
-#                         scale_colour_manual(values=c("black", "orange"),
-#                                                 name="Method",
-#                                                 breaks=as.character(df6$Method),
-#                                                 labels=as.character(df6$Method))+
-# #                         scale_colour_manual(values="black",
-# #                                             name="Cumsum \n diff \n to model",
-# #                                             labels="")+
-#                         
-# #                         geom_point(data=df2, aes(x=date, y=cumsum), colour="blue")+
-# #                         geom_line(data=df2, aes(x=date, y=cumsum), colour="blue", size=0.5)+
-# #                         scale_colour_manual(values="green",
-# #                                             name="Cumsum \n diff \n to model",
-# #                                             labels="")+
-#                         
-#                         coord_cartesian(ylim = c(-1200, 1200))+       
-#                         geom_line(aes(x,y, linetype=Mngt), colour='blue', size = 0.5, vertdf)+
-#                         geom_line(aes(x, pos), linetype="dashed", colour='red', size=0.5, hordf)+
-#                         geom_line(aes(x, neg), linetype="dashed", colour='red', size=0.5, hordf)+
-#                         geom_hline(yintercept=0)+
-#                         annotate("text", min(df2[,1])+1000, hordf[1,1]+95, label = "Control Limits",
-#                                  size=2.5)+
-#                         theme_bw()+
-#                         theme(axis.text.y = element_text(angle=90))+
-#                         xlab("")+
-#                         ylab("Cumulative Sum")
-# 
-#                         filename<-paste0(sname.i, "_orig_cusum_wil_comparison_", stdev, out)
-#                         ggsave(file=filename, p5, width = 24, height = 13.5, units = "cm")
-                
 
         }
 }
